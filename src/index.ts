ce6e50e29e16b11ed6ebc696dbed0335f78d4f91
BSS ZU <274620705z@gmail.com>
2024-04-10 12:51:06 +0800

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-export const one = 1
-export const two = 2
+import Web3 from 'web3'
+
+class EtherTools {
+  private web3: Web3
+
+  constructor(providerUrl: string) {
+    this.web3 = new Web3(providerUrl)
+  }
+
+  async getBalance(address: string): Promise<string> {
+    const balance = await this.web3.eth.getBalance(address)
+    return this.web3.utils.fromWei(balance, 'ether')
+  }
+
+  async sendEther(senderPrivateKey: string, receiverAddress: string, amount: string): Promise<string> {
+    const transaction = {
+      to: receiverAddress,
+      value: this.web3.utils.toWei(amount, 'ether'),
+    }
+
+    const account = this.web3.eth.accounts.privateKeyToAccount(senderPrivateKey)
+    const signedTransaction = await account.signTransaction(transaction)
+    const receipt = await this.web3.eth.sendSignedTransaction(signedTransaction.rawTransaction as string)
+
+    return receipt.transactionHash.toString()
+  }
+}
+
+export default EtherTools
